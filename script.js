f55019cc0feec829bad3ffa7e6fa155affb0a088
Vovegog <daniel.viggen@hotmail.com>
2024-06-06 14:15:09 +0200

    Added a UTC clock and unique ID's to all table-rows

--- script.js
+++ script.js
@@ -2,8 +2,38 @@
 // These cookies should expire at 00:00 UTC on the following day, as that is when reset happens.
 // Additionally, marked checkboxes and their corresponding table-row should have a dash through them, and be highlighted.
 
+function Clock() {
+    var today = new Date();
+    var h = today.getUTCHours();
+    var m = today.getUTCMinutes();
+    var s = today.getUTCSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('Clock').innerHTML = h + ":" + m + ":" + s;
+    var t = setTimeout(function() {
+        Clock()
+    }, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }; // add zero in front of numbers < 10
+    return i;
+}
+
+Clock();
+
 // Get all the checkboxes, and add an event listener to each one.
 var checkboxes = document.getElementsByClassName("checkbox");
+
+// Need to add a unique ID to each row in the table
+var rows = document.getElementsByTagName("tr");
+for (var i = 0; i < rows.length; i++) {
+    rows[i].id = "row" + i;
+}
+
 for (var i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener("click", function() {
         // If the box gets checked, console.log it out, and add it as a cookie
